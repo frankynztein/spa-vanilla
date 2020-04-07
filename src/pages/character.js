ace7bdd4f0252932d0c15d4a99e451aff977e29a
frankynztein <arellanfranciree@gmail.com>
2020-04-07 12:15:46 -0500

    SPA Vanilla

---
+++ src/pages/character.js
@@ -0,0 +1,27 @@
+import getHash from '../utils/getHash';
+import getData from '../utils/getData';
+
+const Character = async() => {
+    const id = await getHash();
+    const character = await getData(id);
+    const view = `
+    <div class="characters-inner">
+      <article class="characters-card">
+        <img src="${character.image}" alt="${character.name}">
+        <h2>${character.name}</h2>
+      </article>
+      <article class="characters-card">
+        <h3>Episodios: <span>${character.episode.length}</span></h3>
+        <h3>Status: <span>${character.status}</span></h3>
+        <h3>Especie: <span>${character.species}</span></h3>
+        <h3>Género: <span>${character.gender}</span></h3>
+        <h3>Origen: <span>${character.origin.name}</span></h3>
+        <h3>Última ubicación: <span>${character.location.name}</span></h3>
+      </article>
+    </div>
+  `;
+
+    return view;
+};
+
+export default Character;
---
+++ src/templates/header.js
@@ -0,0 +1,20 @@
+const Header = () => {
+    const view = `
+    <div class="header-main">
+      <div class="header-logo">
+        <h1>
+          <a href="/">100tifi.co</a>
+        </h1>
+      </div>
+    </div>
+    <div class="header-nav">
+      <a href="#/about/">
+        About
+      </a>
+    </div>
+  `;
+
+    return view;
+};
+
+export default Header;
